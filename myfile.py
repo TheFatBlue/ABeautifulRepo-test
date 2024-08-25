1ebbe5db8f7ce537554a45492d83e43e7f88aad7
TheFatBlue <85777389+TheFatBlue@users.noreply.github.com>
2024-08-25 19:51:38 +0000

    step 11 completed

--- myfile.py
+++ myfile.py
@@ -1,14 +1,14 @@
-# calculate the sum of two numbers
+# Calculate the sum of two numbers
 
 def add_numbers(a, b):
     return a + b
 
 
 if __name__ == "__main__":
-    # input the two numbers
+    # Input the two numbers
     num1, num2 = input("Input the two numbers to be sumed up:\n")
 
-    # calculate the sum and output
+    # Calculate the sum and output
     result = add_numbers(num1, num2)
     print("The sum is:", result)
 
